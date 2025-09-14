720702fe489355e1575f77f4fc9ca16e72b96780
name <raleksandrovic619@gmail.com>
2025-09-14 15:35:20 +0300

    таблица

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import Label, PhotoImage
+from connect import App
 
 root = tk.Tk()
 root.title('Компания обоев')
@@ -16,5 +17,40 @@ logo = logo.subsample(20)
 logo_label = Label(shapka, image = logo , bg= '#405C73')
 logo_label.place(x=5, y=5)
 
-root.mainloop()
+def mat():
+    material = App.select()
+    return material
 
+Oboi = mat()
+
+y_position = 80
+
+for i in Oboi:
+    zone = Label(root, bg = '#BFD5F6', width= 80, height= 7)
+    zone.place(x=15, y= y_position)
+
+    type = Label(zone, bg= '#BFD5F6',
+                 text= f'{i.type} | {i.name}', font=('Constantia', 12))
+    type.place(x=15, y=5)
+
+    article = Label(zone, bg= '#BFD5F6',
+                    text= f'Артикул: {i.article}', font=('Constantia', 10))
+    article.place(x=15, y= 30)
+
+    min_price = Label(zone, bg= '#BFD5F6' ,
+                      text= f'Минимальная стоимость для партнера: {i.min_price} руб.', font=('Constantia', 10))
+    min_price.place(x=15, y= 50)
+
+    width = Label(zone, bg= '#BFD5F6',
+                    text= f'Ширина: {i.width} м', font=('Constantia', 10))
+    width.place(x=15, y= 70)
+
+    price = Label(zone, bg= '#BFD5F6',
+                    text= f'Стоимость: {i.price} руб.', font=('Constantia', 12))
+    price.place(x=360, y= 5)
+                  
+    y_position += 130
+
+
+
+root.mainloop()
